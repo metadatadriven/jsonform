b3b7504aefacb5cf597e45ce401898d0b6590248
stuart malcolm <stuart.malcolm@veramed.co.uk>
2022-10-29 14:01:36 +0000

    use default index.html

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import http.server
 import socketserver
 PORT = 8888
 Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(("example.html", PORT), Handler)
+httpd = socketserver.TCPServer(("", PORT), Handler)
 httpd.serve_forever()
