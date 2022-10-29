c26667fa7ee86eb0ded33e763e03b8421f969daf
Stuart Malcolm <73795478+metadatadriven@users.noreply.github.com>
2022-10-29 23:29:15 +0100

    Update app.sh

--- app.sh
+++ app.sh
@@ -7,6 +7,6 @@
 # # Python/Flask example
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-export FLASK_APP=app-flask.py
+export FLASK_APP=app.py
 #pip install -r requirements_apps.txt --user
 python -m flask run --host=0.0.0.0 --port=8888
